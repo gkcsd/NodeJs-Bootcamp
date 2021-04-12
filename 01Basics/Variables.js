d95f9ef7f705a7986bb3e167c358a6e399a1c5db
gkcsd <kaleg2025@gmail.com>
2021-04-12 15:56:21 +0530

    variables section added

---
+++ 01Basics/Variables.js
@@ -0,0 +1,18 @@
+let name = 'John'
+
+let score = 300
+let bonus = 200
+
+let totalScore = score + bonus;
+
+console.log(totalScore);
+
+/////////////////////////////
+
+let firstName = 'Ganesh'
+let lastName = 'Kale'
+
+let fullName = firstName + ' ' + lastName
+
+console.log(firstName + '-' + lastName)
+console.log(fullName)

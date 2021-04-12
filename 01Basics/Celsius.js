8a1eb7c0a4568bd9a8e00196c3a3c5ed432f6de5
gkcsd <kaleg2025@gmail.com>
2021-04-12 17:14:03 +0530

    Fah to cel conversion & numbers Added

---
+++ 01Basics/Celsius.js
@@ -0,0 +1,20 @@
+let score = 100
+
+score = 130
+
+const bonus = 50
+
+let totalScore = (score + bonus) * 1.8
+
+let complex = (((10 + 10) * 5)/2)
+
+console.log(totalScore)
+
+
+let tempInFahrenheit = 100
+
+//Coding
+
+let tempInCelsius = ((tempInFahrenheit - 32) * 5 / 9)
+
+console.log(tempInCelsius)

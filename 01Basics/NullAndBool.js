026a394f862bfbff7e7c10b92b89257cc5dfd6a6
gkcsd <kaleg2025@gmail.com>
2021-04-13 21:28:26 +0530

    conditional programming using boolean

--- 01Basics/NullAndBool.js
+++ 01Basics/NullAndBool.js
@@ -1,4 +1,14 @@
 //Let us assume that this temp value coming from the websiter API's
-let temp;
+let temp
 
-console.log("The current temperature is: " + temp);
+console.log("The current temperature is: " + temp)
+
+//////////////////////////////////////////////
+
+//Boolean values
+
+let actualMarks = 7
+
+let myGrade = (actualMarks < 10)
+
+console.log(myGrade)

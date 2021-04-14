da63047b904cccf6dea2445acdd92899bedf5cdf
gkcsd <kaleg2025@gmail.com>
2021-04-14 23:57:37 +0530

    if else nesting example added

--- 01Basics/LearnIfElse.js
+++ 01Basics/LearnIfElse.js
@@ -1,5 +1,9 @@
 
-if (true) {
+if (false) {
     console.log("I am inside the if block")
     console.log("I am still inside the if block")
+} else if(true) {
+    console.log("I am inside the else if block")
+} else {
+    console.log("Notice this block")
 }

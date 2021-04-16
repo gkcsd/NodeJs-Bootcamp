136f9667c7bd49c504906730a78db5b660c31bfb
gkcsd <kaleg2025@gmail.com>
2021-04-16 23:39:10 +0530

    ifelse ladder example added

--- 01Basics/LearnIfElse.js
+++ 01Basics/LearnIfElse.js
@@ -7,3 +7,17 @@ if (false) {
 } else {
     console.log("Notice this block")
 }
+
+/////////////////////////////////
+
+var whoIsHere = 'user'
+
+if (whoIsHere === 'user') {
+    console.log("Greeting message for user")
+    console.log("Allow access to view all courses")
+} else if (whoIsHere == 'teacher') {
+    console.log("Greeting message for teacher")
+    console.log("Allow access to his courses")
+} else {
+    console.log("MESSAGE: Please verify your email")
+}

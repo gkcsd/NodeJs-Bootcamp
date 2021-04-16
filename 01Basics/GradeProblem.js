8f5b700b3cc4d517ca21714ebb543c30d0ee844a
gkcsd <kaleg2025@gmail.com>
2021-04-17 00:03:14 +0530

    Grade example added

---
+++ 01Basics/GradeProblem.js
@@ -0,0 +1,15 @@
+//Grade problem-
+
+var marks = 5
+
+if (marks == 10) {
+    console.log("Amazing Score")
+} else if ( marks == 5) {
+    console.log("Good Score")
+} else if (marks == 4) {
+    console.log("Poor Score")
+} else if (marks == 0) {
+    console.log("Fail")
+} else {
+    console.log("Please Enter valid marks...")
+}

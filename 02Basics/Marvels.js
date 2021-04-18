324ff598ce06205ccad811ddbc9712307e3506f3
gkcsd <kaleg2025@gmail.com>
2021-04-18 23:21:50 +0530

    Array example added

---
+++ 02Basics/Marvels.js
@@ -0,0 +1,7 @@
+
+const superHeroes = ['Superman', 'Iron-Man', 'Capt. America']
+
+console.log(superHeroes)
+console.log(superHeroes[0])
+console.log(superHeroes[1])
+console.log(superHeroes[superHeroes.length - 1])

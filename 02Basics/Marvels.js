6f0d16c82e196fc3fe04d9e234dc1baf91811b59
gkcsd <kaleg2025@gmail.com>
2021-04-18 23:33:09 +0530

    String literal addes backtick

--- 02Basics/Marvels.js
+++ 02Basics/Marvels.js
@@ -1,7 +1,9 @@
 
-const superHeroes = ['Superman', 'Iron-Man', 'Capt. America']
+const superHeroes = ['Spiderman', 'Iron-Man', 'Capt. America', 'Thor']
 
 console.log(superHeroes)
 console.log(superHeroes[0])
 console.log(superHeroes[1])
 console.log(superHeroes[superHeroes.length - 1])
+console.log()
+console.log(`We have ${superHeroes.length} super heroes`)

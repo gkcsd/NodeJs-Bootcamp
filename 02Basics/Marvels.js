e4cb5c87e33e4265a7d618c6e488750bd86d0cf8
gkcsd <kaleg2025@gmail.com>
2021-05-01 15:11:31 +0530

    array and string literals added

--- 02Basics/Marvels.js
+++ 02Basics/Marvels.js
@@ -7,3 +7,36 @@ console.log(superHeroes[1])
 console.log(superHeroes[superHeroes.length - 1])
 console.log()
 console.log(`We have ${superHeroes.length} super heroes`)
+
+
+console.log()
+//Three must know methods for array in javascript
+
+const numbers = ['One', 'Two', 'Three', 'Four', 'Five', 'Six']
+
+// numbers[1] = 'SOMETHING'
+
+// console.log(numbers)
+
+console.log()
+//Start 
+// console.log(numbers.shift())
+// console.log(numbers)
+console.log()
+
+console.log(numbers.unshift('SOMETHING'))
+console.log(numbers)
+
+console.log()
+//End
+console.log('Numbers is to be deleted is: ' + numbers.pop())
+console.log(numbers)
+console.log()
+
+numbers.push('Seven')
+console.log(numbers)
+console.log()
+
+//Middle
+numbers.splice(2, 2, "SOMETHINGMORE")
+console.log(numbers)

b8c7e61bfc5d9a21b13e8eecf05cb6dd3213f0c1
gkcsd <kaleg2025@gmail.com>
2021-05-08 23:14:33 +0530

    simple todo app added

--- 01Intermediate/Trello.js
+++ 01Intermediate/Trello.js
@@ -12,3 +12,32 @@ console.log()
 for(let i = 0; i < days.length; i++) {
     console.log(days[i])
 }
+
+
+console.log()
+//////////////////Simple todo application
+
+let todos = []
+
+todos.push('Gym')
+todos.push('Work')
+todos.push('Coding')
+
+todos.forEach(function(todo, index){
+    console.log(`Your task no. ${index} is ${todo}`)
+})
+
+
+console.log()
+//////////////todo applicatioin using for loop and push
+
+
+let myTodos = []
+
+myTodos.unshift('Dinner')
+myTodos.unshift('Study')
+myTodos.unshift('Exercise')
+
+for(let i = 0; i < myTodos.length; i++) {
+    console.log(`Your task no. ${i} is ${myTodos[i]}`)
+}

1d6eb1f14ddb4c83487b7d4c17663f3cbb02a3bc
gkcsd <kaleg2025@gmail.com>
2021-05-08 22:58:40 +0530

    For loop added

--- 01Intermediate/Trello.js
+++ 01Intermediate/Trello.js
@@ -3,3 +3,12 @@ const days = ['Mon', 'Tue', 'Wed', 'Thur', 'Frid', 'Sat']
 days.forEach(function(day,index) {
     console.log(`Day starts with ${index+1} ---- ${day}`)
 })
+
+console.log()
+////////////////////////////////
+//for loop
+
+
+for(let i = 0; i < days.length; i++) {
+    console.log(days[i])
+}

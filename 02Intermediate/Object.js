06f117004cb567159ebfd75c7da2d0e75b2b35e6
gkcsd <kaleg2025@gmail.com>
2021-05-09 10:55:25 +0530

    object example added

---
+++ 02Intermediate/Object.js
@@ -0,0 +1,8 @@
+let myYoutubeVideos = {
+    title: 'Loops in javascript',
+    lengthOfVideo: 15,
+    authorName: 'Ganesh Kale',
+    videoDescription: 'This video is about different loops in javascript'
+}
+
+console.log(`Todays teaching agenda is ${myYoutubeVideos.title} and its by ${myYoutubeVideos.authorName} `)

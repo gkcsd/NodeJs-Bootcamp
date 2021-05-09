e859429aa2ffdb9661b7d26b9ab3c085a7af63b3
gkcsd <kaleg2025@gmail.com>
2021-05-09 11:22:32 +0530

    objectInFunction added

--- 02Intermediate/Object.js
+++ 02Intermediate/Object.js
@@ -7,3 +7,39 @@ let myYoutubeVideos = {
 
 console.log(myYoutubeVideos)
 console.log(`Todays teaching agenda is ${myYoutubeVideos.title} and its by ${myYoutubeVideos.authorName} `)
+
+console.log()
+//////////////////////////
+//change is object
+
+myYoutubeVideos.title = 'forEach loop in javascipt'
+
+console.log(myYoutubeVideos)
+console.log(`Todays teaching agenda is ${myYoutubeVideos.title} and its by ${myYoutubeVideos.authorName} `)
+
+
+console.log()
+///////////////////////////
+//Objects in function
+
+let myYoutubeVideosTwo = {
+    title: 'Loops in javascript',
+    lengthOfVideo: 15,
+    authorName: 'Ganesh Kale',
+    videoDescription: 'This video is about different loops in javascript'
+}
+
+let changeVideoLength = function(myObject){
+    console.log(`My YouTube video length is ${myObject.lengthOfVideo}`)
+}
+
+changeVideoLength(myYoutubeVideosTwo)
+
+console.log()
+/////using return keyword this one use more functionality
+
+let changeVideoLengthTwo = function(myObject){
+    return `My YouTube video length is ${myObject.lengthOfVideo + 2}`
+}
+
+console.log(changeVideoLengthTwo(myYoutubeVideosTwo))

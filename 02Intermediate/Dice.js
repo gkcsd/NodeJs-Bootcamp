163a397bd8bf11e69f9d229790803ada5058f918
gkcsd <kaleg2025@gmail.com>
2021-05-10 23:33:01 +0530

    Math functions added

---
+++ 02Intermediate/Dice.js
@@ -0,0 +1,15 @@
+let num1 = 22
+let num2 = 7
+
+let result = num1/num2
+
+console.log(result.toFixed(2))
+
+let floatRes = result.toFixed(2)
+
+console.log(Math.floor(floatRes))
+console.log(Math.ceil(floatRes))
+
+console.log()
+
+console.log(Math.random() * 10)

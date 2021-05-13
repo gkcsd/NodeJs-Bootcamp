bacbc606e2baeb7105150c9f0b6c29fb95f5706b
gkcsd <kaleg2025@gmail.com>
2021-05-13 21:03:14 +0530

    Coercion example added

---
+++ 01Advance/Coercion.js
@@ -0,0 +1,25 @@
+// console.log('5' - 5)
+
+// const type = typeof(5)
+// console.log(type)
+
+// const adder = true + 5
+// console.log(adder)
+
+const loginDetails = []
+
+const loginID = loginDetails[0]
+
+if(loginID !== undefined) {
+    console.log("Allow user to login")
+}else {
+    console.log("Authintication failed")
+}
+
+// //values that interpret as false:
+
+// false
+// 0
+// null
+// ''
+// undefined

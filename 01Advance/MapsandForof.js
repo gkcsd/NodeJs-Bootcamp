4a04e98cd73832138f9d905e862f162d7b26a08c
gkcsd <kaleg2025@gmail.com>
2021-05-15 23:56:10 +0530

    forofoloop added

--- 01Advance/MapsandForof.js
+++ 01Advance/MapsandForof.js
@@ -22,4 +22,28 @@ users.set('john', john)
 users.set('marry', marry)
 users.set('sam', sam)
 
-console.log(users)
+// console.log(users.size)
+
+// console.log(users.get('john'))
+
+// console.log(users.keys())
+// console.log(users.values())
+
+// for of loop in JS
+// for(const key of users.keys()) {
+//     console.log(key)
+// }
+
+
+// for(const [key, value] of users.entries()) {
+//     // console.log(key + " = " + value.name)
+//     return `${key} = ${value.name}`
+// }
+
+// users.forEach((value, key) => {
+//     console.log(key + " = " + value.name)
+// })
+
+let result = users.forEach((value, key) => {
+    return `${key} = ${value.name}`
+})

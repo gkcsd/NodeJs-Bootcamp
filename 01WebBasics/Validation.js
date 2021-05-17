247095f1e27ae02c9637aeac57cd26fad92853e4
gkcsd <kaleg2025@gmail.com>
2021-05-17 23:59:09 +0530

    form validation added

--- 01WebBasics/Validation.js
+++ 01WebBasics/Validation.js
@@ -8,3 +8,15 @@ function myValidation() {
         para.textContent = myValue
     }
 }
+
+
+
+//Form validation..
+
+document.querySelector('.myform').addEventListener('submit', (event) => {
+    event.preventDefault()
+    console.log(event.target.username.value)
+    console.log(event.target.realname.value)
+    event.target.username.value = ''
+    event.target.realname.value = ''
+})

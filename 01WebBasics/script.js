c2fef24af9fe46e2a348e2d5913cdf02abbbf94f
gkcsd <kaleg2025@gmail.com>
2021-05-17 18:07:24 +0530

    event listner added

--- 01WebBasics/script.js
+++ 01WebBasics/script.js
@@ -16,3 +16,7 @@ const myNewPara = document.createElement('p')
 myNewPara.textContent = 'I was added via JS'
 
 document.querySelector('body').appendChild(myNewPara)
+
+document.querySelector('button').addEventListener('click', (event) => {
+    event.target.textContent = 'I was Clicked'
+})

c0ab22d7aebd1c68e1c3e008012b1bd3f01aaae6
gkcsd <kaleg2025@gmail.com>
2021-05-17 14:21:43 +0530

    forEach loop for querySelectorALL added

--- 01WebBasics/script.js
+++ 01WebBasics/script.js
@@ -1,5 +1,13 @@
 // console.log(document.getElementById('idone'))
 // console.log(document.getElementsByClassName('classone'))
 
-const myElement = document.querySelectorAll('#idone')
-console.log(myElement[0])
+// const myElement = document.querySelectorAll('#idone')
+// console.log(myElement[0])
+
+// const myPElement = document.querySelector('p')
+// myPElement.textContent = 'I am being changed by JS'
+
+const myPElement = document.querySelectorAll('p')
+myPElement.forEach((p) => {
+    p.textContent = 'I am changed using loop in JS'
+})

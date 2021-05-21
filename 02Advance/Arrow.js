772ab45eb30a5d48b6f3e2ec5e5077a6f018e27d
gkcsd <kaleg2025@gmail.com>
2021-05-21 16:48:54 +0530

    Arrow function added

---
+++ 02Advance/Arrow.js
@@ -0,0 +1,25 @@
+
+// const sayHello = function(name) {
+//     return "Hello, there " + name + " !"
+// }
+
+// console.log(sayHello("Sammy"))
+
+
+const sayHello = (name) => `Hello, there ${name} ! how are you ?`
+console.log(sayHello("Sammy"))
+
+
+const todos = [{
+    title: "Go to gym",
+    isDone: true
+}, {
+    title: "Buy a Milk",
+    isDone: false
+}, {
+    title: "Go to work",
+    isDone: true
+}]
+
+const thingsDone = todos.filter((todo) => todo.isDone === true)
+console.log(thingsDone)
